c1c0afd59976fca22fddf3dfdb5663fc87d020ec
LouisVanhove <vanhovelouis123@gmail.com>
2025-06-20 09:17:34 +0200

    Making the homepage look more clean and scalable
    This also has a link to a button to the map page, which will be added next commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,29 @@
-import Image from "next/image";
+'use client';
+import Link from 'next/link';
+// import Image from "next/image";
 
-export default function HomePage() {
+// Home can be named anything, this just makes sense
+export default function Home() {
   return (
-    <main style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <h1>Hello world!</h1>
+    <main
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        gap: '1rem',
+        padding: '2rem'
+      }}
+    >
+      <h1>Welcome to HikeMap</h1>
+
+      <Link href="/map">
+        <button>
+          Go to Map
+        </button>
+      </Link>
     </main>
   );
 }
