04891ca43f6a36ce66f062f448348262756515e1
LouisVanhove <vanhovelouis123@gmail.com>
2025-06-20 09:26:57 +0200

    Making the map dynamic and disabling SSR for vercel
    Vercel tried to optimize it into a static page, but leaflet is using things like 'window' under the hood, which it cannot statically get, thus, we disable server side rendering - let the client generate it on runtime.

---
+++ frontend/src/app/map/map_client.tsx
@@ -0,0 +1,22 @@
+'use client';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { LatLngExpression } from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const position: LatLngExpression = [51.505, -0.09];
+
+export default function MapClient() {
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={true} style={{ height: '100%', width: '100%' }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>🌍 You are here!</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+}
--- frontend/src/app/map/page.tsx
+++ frontend/src/app/map/page.tsx
@@ -1,24 +1,11 @@
 'use client';
+import dynamic from 'next/dynamic';
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { LatLngExpression } from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+// Dynamically import the MapClient component with SSR disabled
+const Map = dynamic(() => import('./map_client'), {
+  ssr: false,
+});
 
-const position: LatLngExpression = [51.505, -0.09];
-
-// Map can be named anything, this just makes sense
-export default function Map() {
-  return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <MapContainer center={position} zoom={13} scrollWheelZoom={true} style={{ height: '100%', width: '100%' }}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>🌍 You are here!</Popup>
-        </Marker>
-      </MapContainer>
-    </div>
-  );
+export default function MapPage() {
+  return <Map />;
 }
