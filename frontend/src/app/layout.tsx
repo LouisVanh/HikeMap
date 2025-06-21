3cd64d3415700e56b7925ab51767cf9b85145ec7
LouisVanhove <vanhovelouis123@gmail.com>
2025-06-21 07:31:10 +0200

    Cursive nice font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,31 @@
 import './globals.css';
 import 'leaflet/dist/leaflet.css';
+import { Playfair_Display, Dancing_Script, Cormorant_Garamond, Marcellus } from 'next/font/google';
+
+// Import all fonts as CSS variables
+const playfair = Playfair_Display({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-playfair',
+});
+
+const dancing = Dancing_Script({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-dancing',
+});
+
+const cormorant = Cormorant_Garamond({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-cormorant',
+});
+
+const marcellus = Marcellus({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-marcellus',
+});
 
 export const metadata = {
   title: 'HikeMap',
@@ -8,7 +34,13 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en"       
+    className={`
+        ${playfair.variable}
+        ${dancing.variable}
+        ${cormorant.variable}
+        ${marcellus.variable}
+      `}>
       <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
