a32aec119fb5877bd8c487e0b31201bfbef5a523
LouisVanhove <vanhovelouis123@gmail.com>
2025-07-02 02:32:30 +0200

    Sitemap generation for SEO

---
+++ frontend/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://hikemap.app',
+  generateRobotsTxt: true,
+  outDir: './public', // this will put sitemap.xml and robots.txt in /public
+};
