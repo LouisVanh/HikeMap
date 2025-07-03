8cd886515b12d0dc3f8eea5200c5a636fa634315
LouisVanhove <vanhovelouis123@gmail.com>
2025-07-03 04:52:10 +0200

    Supabase auth persisting in prod

--- frontend/src/utils/supabaseClient.ts
+++ frontend/src/utils/supabaseClient.ts
@@ -6,4 +6,10 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 // Sets up HTTP request, passing authorization and api key
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        persistSession: true,
+        autoRefreshToken: true,
+        detectSessionInUrl: true,
+    },
+});
