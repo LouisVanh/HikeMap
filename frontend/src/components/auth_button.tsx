99836bc7757f35360e09b82db59dfbe07acf9657
LouisVanhove <vanhovelouis123@gmail.com>
2025-07-05 01:47:55 +0200

    Typescript dependency fixes in auth_button

--- frontend/src/components/auth_button.tsx
+++ frontend/src/components/auth_button.tsx
@@ -3,19 +3,21 @@
 import React, { useEffect, useState } from "react";
 import { createClient } from "@/utils/supabase/client";
 import { signOut, signInWithGoogle } from "@/utils/supabase/client_auth";
+import { User } from "@supabase/supabase-js";
 
 export const AuthButton = () => {
-  const [user, setUser] = useState<any>(null);
+  const [user, setUser] = useState<User|null>();
   const supabase = createClient();
   useEffect(() => {
     const fetchUser = async () => {
       const {
         data: { user },
       } = await supabase.auth.getUser();
+      if(user !== null)
       setUser(user);
     };
     fetchUser();
-  }, []);
+  }, [supabase.auth]);
   if (user) {
     return (
       <div className="flex items-center gap-4">
